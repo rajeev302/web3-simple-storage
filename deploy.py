b58255fc15d49f5854aabc429991062b9f2147ec
rajvg15 <>
2022-02-23 13:20:52 +0530

    added transaction to ganache chain

--- deploy.py
+++ deploy.py
@@ -5,7 +5,6 @@ from solcx import compile_standard, install_solc
 
 with open("./SimpleStorage.sol", "r")  as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
 
     print("Installing...")
     install_solc("0.8.0")
@@ -32,3 +31,32 @@ with open("compiled_code.json", "w") as file:
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
 # get abi
 abi = json.loads(compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"])["output"]["abi"]
+
+# For connecting to ganache
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 1337
+my_address = "0xF214884bD29fcd86fA62C58932c89a68ebd48096"
+private_key = "87aef13d8093067b31587332757d01d0a660f1f6b410d16548a71a42a4c5427a"
+
+# Create the contract in Python
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
+# Submit the transaction that deploys the contract
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+# Sign the transaction
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print("Deploying Contract!")
+# Send it!
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Wait for the transaction to be mined, and get the transaction receipt
+print("Waiting for transaction to finish...")
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
